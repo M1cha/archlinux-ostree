5af720921a23d555811cd14fbab52a8485a5428f
Your Name <you@example.com>
2023-12-31 09:58:01 +0100

    doc

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # archlinux-ostree
 
-This is a utility that simplifies building an ostree image using pacstrap.
+A tool for building an archlinux OSTree tree and deploying it.
+
+If you just want to know how to use this, check out the
+[guide on the Arch wiki](https://wiki.archlinux.org/title/Install_Arch_Linux_inside_OSTree).
+
+## Thanks
+- [GrabbenD](https://github.com/GrabbenD) for making this work with mainline
+  GRUB2 and for making this work without Fedora.
+- [ostreedev](https://github.com/ostreedev) for the awesome OSTree project.
+- [Arch Linux](https://archlinux.org) for an awesome Linux distro.
+- [containers](https://github.com/containers) for podman.
+
+## References
+- [OSTree documentation](https://github.com/containers)
--- lib/common.sh
+++ lib/common.sh
@@ -1,4 +1,5 @@
 
+# Source: https://github.com/archlinux/arch-install-scripts/blob/4802e1197e43e300e8fc545b7b2a0358ad2f920b/common#L75
 # shellcheck disable=SC2059 # $1 and $2 can contain the printf modifiers
 out() { printf "$1 $2\n" "${@:3}"; }
 error() { out "====> ERROR:" "$@"; } >&2
@@ -11,10 +12,12 @@ arg_to_varname() {
 	echo "${name//-/_}"
 }
 
+# Source: https://gitlab.archlinux.org/pacman/pacman/-/blob/v6.0.2/scripts/libmakepkg/util/pkgbuild.sh.in?ref_type=tags#L29
 have_function() {
 	declare -f "$1" >/dev/null
 }
 
+# Source: https://github.com/archlinux/arch-install-scripts/blob/4802e1197e43e300e8fc545b7b2a0358ad2f920b/common#L183
 # This outputs code for declaring all variables to stdout. For example, if
 # FOO=BAR, then running
 #     declare -p FOO
@@ -32,6 +35,7 @@ declare_all() {
   declare -pf
 }
 
+# Source: https://stackoverflow.com/questions/1527049/how-can-i-join-elements-of-a-bash-array-into-a-delimited-string/17841619#17841619
 join_by() {
 	local IFS="$1"
 	shift
